2fbda5550158d33c36a629d6ab15180c65ab919e
Nedzib Sastoque <bsastoque@unal.edu.co>
2019-05-12 11:48:40 -0500

    add last String operators

--- 0x00 Python Basics/2-stringOperators.py
+++ 0x00 Python Basics/2-stringOperators.py
@@ -8,18 +8,27 @@ msg = a*3 #copia 3 veces el contenido de a y los concatena
 
 #Añadir
 msg = a
-msg +=b #
+msg +=b #concatena a con b
 
 #Métodos para cadenas de caracteres: buscar, cambiar
 #Extensión
+tam = len(a) #devuelve el tamaño de a
 
 #Encontrar
+find = a.find("l") # '2' devuelve el indice de donde esta lo buscado en la cadena
+find = b.find("ld") # '3' si lo buscado no existe, devuelve '-1'
 
-#Minúsculas
+#Minúsculas-Mayúsculas
+msg = a.upper() #pasa todo lo de a a mayusculas
+msg = b.lower() #
 
 #Reemplazar
+msg = a
+msg.replace("l", "pizza") # reemplaza 'l' por 'pizza' en el string
 
 #Cortar
+msg = a[1:2] # 'e' el primer numero recorta desde el inicio el otro desde el final
 
 #Secuencias de escape
-
+#\t añade tabulacion
+#\n salto de linea
